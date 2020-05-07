b27ee36344f8e4bdb54eb0ff73aed897237168f4
qiqf <723469354@qq.com>
2020-05-07 22:53:37 +0800

    feat(init): 初始化项目和搭建typescript webpack 开发环境

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/**
+ * Created by qiqf on 2020/5/7
+ */
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: process.env.NODE_ENV === 'production' ? false : 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    stats: 'errors-only',
+    compress: false,
+    host: 'localhost',
+    port: 8089
+  },
+  plugins: [
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: ['./dist']
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/index.html'
+      })
+  ]
+};
