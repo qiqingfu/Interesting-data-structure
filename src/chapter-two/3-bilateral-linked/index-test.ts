b4a0260e4c599830ecd2fa14e0a74912188c9209
qiqf <723469354@qq.com>
2020-05-13 22:15:49 +0800

    feat(bilateral-linked): 双向链表根据指定下标插入元素

--- src/chapter-two/3-bilateral-linked/index-test.ts
+++ src/chapter-two/3-bilateral-linked/index-test.ts
@@ -14,4 +14,11 @@ linked.pushElem(4);
 console.log('向链表中添加一个元素 5');
 linked.pushElem(5);
 
+
+// 向双向链表中指定下标插入一个元素
+console.log('向双向链表中指定下标 2 插入一个元素 6', linked.insertElem(6, 2));
+console.log('向双向链表中指定下标 2 插入一个元素 6', linked.insertElem(7, 0));
+console.log('向双向链表中指定下标 2 插入一个元素 6', linked.insertElem(8, 6));
+
+console.log(linked.getAllElem());
 console.log(linked);
--- src/chapter-two/3-bilateral-linked/index.ts
+++ src/chapter-two/3-bilateral-linked/index.ts
@@ -52,7 +52,53 @@ class Linked {
    * @param index
    */
   insertElem(elem: any, index: number):boolean {
+    if (index < 0 || index > this.length - 1) {
+      throw new Error(`${index} 范围错误, 应该在 0 - ${this.length - 1} 区间`);
+    }
+
+    if (index === 0) {
+      this.pushElem(elem);
+      return true;
+    }
+
+    let head = this.head;
+    let i = 0;
+
+    while (head && i < index) {
+      head = <any>head.next;
+      i++
+    }
+
+    if (!head || i > index) {
+      return false;
+    }
+
+    const newNode = new Node(elem);
+    (head.next as Node<any>).prev = newNode;
+    newNode.next = head.next;
+    newNode.prev = head;
+    head.next = newNode;
+
+    this.length++;
+
     return true;
+  }
+
+  /**
+   * 获取单向链表中所有项
+   */
+  getAllElem():any[] {
+    let p = this.head.next;
+    let result = [];
+    let i = 0;
+
+    while (p) {
+      result[i] = p.data;
+      p = p.next;
+      i++;
+    }
+
+    return result;
   }
 }
 
