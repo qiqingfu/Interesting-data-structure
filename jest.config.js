c27e58babb153e27b339b3293d72f9776e23ea40
qiqf <723469354@qq.com>
2020-06-08 23:04:02 +0800

    chore(jest): 新增 jest 测试

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by qiqf on 2020/6/8
+ */
+
+module.exports = {
+  roots: [
+    "<rootDir>/test"
+  ],
+  testRegex: 'test/(.+)\\.test\\.(jsx?|tsx?)$',
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
