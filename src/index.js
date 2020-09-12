44893e8ff2373353840bba5182ecad4a4cde16f6
qiqingfu <9116895@qq.com>
2020-09-12 17:49:05 +0800

    chore: 重新搭建 js 的webpack运行环境

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by qiqf on 2020/9/12
+ * entry
+ */
+
+console.log("entry")
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/**
+ * Created by qiqf on 2020/9/12
+ */
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const OUTPUT_FILEPATH = "dist"
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, OUTPUT_FILEPATH),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, OUTPUT_FILEPATH),
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+}
