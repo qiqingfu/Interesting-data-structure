1879bf0947ae743c626c8abd3d5346c5619c9d36
qiqingfu <9116895@qq.com>
2020-10-27 14:02:50 +0800

    feat(chapter3): 链队列数据结构

---
+++ src/chapter3/04-chain-queue/index.js
@@ -0,0 +1,84 @@
+/**
+ * Created by qiqf on 2020/10/27
+ */
+
+/**
+ * 链队列
+ */
+
+/**
+ * export ChainQueue
+ * @type {ChainQueue}
+ */
+module.exports = ChainQueue
+
+/**
+ * Node of the chain queue
+ * @constructor
+ */
+function Node(data, next) {
+  this.data = data
+  this.next = next || null
+}
+
+/**
+ * ChainQueue Constructor
+ * @constructor
+ */
+function ChainQueue() {
+  if (!(this instanceof ChainQueue)) {
+    return new ChainQueue()
+  }
+
+  this.base = new Node('__head__')
+  this.front = this.base
+  this.rear = this.base
+  this.length = 0
+}
+
+/**
+ * an element into the queue
+ *
+ * @param {number} data
+ * @return {boolean}
+ */
+ChainQueue.prototype.push = function push(data) {
+  const newNode = new Node(data)
+  this.rear.next = newNode
+  this.rear = newNode
+  this.length++
+
+  return true
+}
+
+/**
+ * out of the queue an element
+ *
+ * @returns {any|null}
+ */
+ChainQueue.prototype.shift = function shift() {
+  if (this.front === this.rear) return null
+  const p = this.front.next
+  const { data } = p
+  this.front.next = p.next
+  /**
+   * 当链队列中只有一个节点时
+   */
+  if (this.rear === p) {
+    this.front = this.rear
+  }
+  this.length--
+
+  return data
+}
+
+/**
+ * gets the top queue element
+ * @returns {any|number}
+ */
+ChainQueue.prototype.getTop = function getTop() {
+  if (this.front !== this.rear) {
+    return this.front.next.data
+  }
+  return -1
+}
